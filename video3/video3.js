b2434e62b9bd123737dea7e8b62d834b8d0ae350
Trinh Tram Anh <tramanh.912.pch@gmail.com>
2023-03-03 09:28:03 +0700

    init project

---
+++ video3/video3.js
@@ -0,0 +1,12 @@
+let a = 5;
+let d =5.9;
+
+
+const j =5;
+console.log("hello world from javascript")
+// `` this is backtick
+
+let x=5, y=6,z=7;
+console.log("Test String", 'x=',x,'y=', y, 'z=', z)
+
+console.log("Test String2222"+ 'x='+ x +'y='+ y+ 'z='+ z)
---
+++ video7/video7.js
@@ -0,0 +1,11 @@
+console.log("Hello World from JavaScript")
+
+let a=5;
+let b = 10;
+console.log('Fifteen is ' + (a+b) + ' and /nnot' + (2*a+b));
+console.log('Fifteen is ' + a+b + ' and /nnot' + (2*a+b));
+
+// Let work smart
+console.log(`Hoi dan it check string: a = ${a} b = ${b} a+b = ${a+b}`);
+console.log(`/hoidanit/${a}`);
+
